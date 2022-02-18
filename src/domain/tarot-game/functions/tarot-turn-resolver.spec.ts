ef2f7304c03ffe6d07d5b652ce1a1d5828045f99
Emmanuel Colas <emmanuel.colas@bioserenity.com>
2022-02-18 21:24:39 +0100

    Implement tarot turn resolver

---
+++ src/domain/tarot-game/functions/tarot-turn-resolver.spec.ts
@@ -0,0 +1,328 @@
+import {PlayedCard, TurnResult} from "../../card-game/functions/resolve-turn";
+import {
+    HEART_9,
+    JOKER,
+    SPADE_1,
+    SPADE_2,
+    SPADE_3,
+    SPADE_4,
+    SPADE_C,
+    SPADE_Q,
+    TRUMP_1,
+    TRUMP_5
+} from "tarot-card-deck/dist/cards/all-playing-cards";
+import {resolveTarotTurn} from "./tarot-turn-resolver";
+
+describe(`Tarot turn resolver`, () => {
+    test(`Given four spades,
+        when resolving turn,
+        then player that has played the biggest spade wins all the cards`, () => {
+        const playedCards: readonly PlayedCard[] = [
+            {
+                playerIdentifier: "P0",
+                playingCard: SPADE_1
+            },
+            {
+                playerIdentifier: "P1",
+                playingCard: SPADE_2
+            },
+            {
+                playerIdentifier: "P2",
+                playingCard: SPADE_3
+            },
+            {
+                playerIdentifier: "P3",
+                playingCard: SPADE_4
+            },
+        ]
+        const expectedTurnResult: TurnResult = {
+            winner: "P3",
+            wonCardsByPlayer: [
+                {
+                    playerIdentifier: "P0",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P1",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P2",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P3",
+                    wonCards: [SPADE_1, SPADE_2, SPADE_3, SPADE_4]
+                },
+            ]
+        }
+        expect(resolveTarotTurn(playedCards)).toEqual(expectedTurnResult);
+    });
+
+    test(`Given four spades including rider and queen,
+        when resolving turn,
+        then player that has played the biggest spade wins all the cards`, () => {
+        const playedCards: readonly PlayedCard[] = [
+            {
+                playerIdentifier: "P0",
+                playingCard: SPADE_C
+            },
+            {
+                playerIdentifier: "P1",
+                playingCard: SPADE_2
+            },
+            {
+                playerIdentifier: "P2",
+                playingCard: SPADE_3
+            },
+            {
+                playerIdentifier: "P3",
+                playingCard: SPADE_Q
+            },
+        ]
+        const expectedTurnResult: TurnResult = {
+            winner: "P3",
+            wonCardsByPlayer: [
+                {
+                    playerIdentifier: "P0",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P1",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P2",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P3",
+                    wonCards: [SPADE_C, SPADE_2, SPADE_3, SPADE_Q]
+                },
+            ]
+        }
+        expect(resolveTarotTurn(playedCards)).toEqual(expectedTurnResult);
+    });
+
+    test(`Given a trump in the played cards,
+        when resolving turn,
+        then the player that has played trump wins all the cards`, () => {
+        const playedCards: readonly PlayedCard[] = [
+            {
+                playerIdentifier: "P0",
+                playingCard: SPADE_1
+            },
+            {
+                playerIdentifier: "P1",
+                playingCard: SPADE_2
+            },
+            {
+                playerIdentifier: "P2",
+                playingCard: TRUMP_1
+            },
+            {
+                playerIdentifier: "P3",
+                playingCard: SPADE_4
+            },
+        ]
+        const expectedTurnResult: TurnResult = {
+            winner: "P2",
+            wonCardsByPlayer: [
+                {
+                    playerIdentifier: "P0",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P1",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P2",
+                    wonCards: [SPADE_1, SPADE_2, TRUMP_1, SPADE_4]
+                },
+                {
+                    playerIdentifier: "P3",
+                    wonCards: []
+                },
+            ]
+        }
+        expect(resolveTarotTurn(playedCards)).toEqual(expectedTurnResult);
+    });
+    test(`Given 2 trumps in played cards,
+        when resolving turn,
+        then the player that has played the biggest trump wins all the cards`, () => {
+        const playedCards: readonly PlayedCard[] = [
+            {
+                playerIdentifier: "P0",
+                playingCard: SPADE_1
+            },
+            {
+                playerIdentifier: "P1",
+                playingCard: TRUMP_5
+            },
+            {
+                playerIdentifier: "P2",
+                playingCard: TRUMP_1
+            },
+            {
+                playerIdentifier: "P3",
+                playingCard: SPADE_4
+            },
+        ]
+        const expectedTurnResult: TurnResult = {
+            winner: "P1",
+            wonCardsByPlayer: [
+                {
+                    playerIdentifier: "P0",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P1",
+                    wonCards: [SPADE_1, TRUMP_5, TRUMP_1, SPADE_4]
+                },
+                {
+                    playerIdentifier: "P2",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P3",
+                    wonCards: []
+                },
+            ]
+        }
+        expect(resolveTarotTurn(playedCards)).toEqual(expectedTurnResult);
+
+    });
+    test(`Given a spade as first card and a heart bigger than all of the spades,
+        when resolving turn,
+        then the player that has played the biggest spade win all the cards`, () => {
+        const playedCards: readonly PlayedCard[] = [
+            {
+                playerIdentifier: "P0",
+                playingCard: SPADE_1
+            },
+            {
+                playerIdentifier: "P1",
+                playingCard: SPADE_3
+            },
+            {
+                playerIdentifier: "P2",
+                playingCard: HEART_9
+            },
+            {
+                playerIdentifier: "P3",
+                playingCard: SPADE_4
+            },
+        ]
+        const expectedTurnResult: TurnResult = {
+            winner: "P3",
+            wonCardsByPlayer: [
+                {
+                    playerIdentifier: "P0",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P1",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P2",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P3",
+                    wonCards: [SPADE_1, SPADE_3, HEART_9, SPADE_4]
+                },
+            ]
+        }
+        expect(resolveTarotTurn(playedCards)).toEqual(expectedTurnResult);
+    });
+    test(`Given 3 spades and excuse,
+        when resolving turn,
+        then the player that has played the biggest spade wins all the spades and the player that has played excuse keep it`, () => {
+        const playedCards: readonly PlayedCard[] = [
+            {
+                playerIdentifier: "P0",
+                playingCard: SPADE_1
+            },
+            {
+                playerIdentifier: "P1",
+                playingCard: SPADE_3
+            },
+            {
+                playerIdentifier: "P2",
+                playingCard: JOKER
+            },
+            {
+                playerIdentifier: "P3",
+                playingCard: SPADE_4
+            },
+        ]
+        const expectedTurnResult: TurnResult = {
+            winner: "P3",
+            wonCardsByPlayer: [
+                {
+                    playerIdentifier: "P0",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P1",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P2",
+                    wonCards: [JOKER]
+                },
+                {
+                    playerIdentifier: "P3",
+                    wonCards: [SPADE_1, SPADE_3, SPADE_4]
+                },
+            ]
+        }
+        expect(resolveTarotTurn(playedCards)).toEqual(expectedTurnResult);
+    });
+    test(`Given excuse played as first card and 3 spades,
+        when resolving turn,
+        then the player that has played the biggest spade wins all the spades and the player that has played excuse keep it`, () => {
+        const playedCards: readonly PlayedCard[] = [
+            {
+                playerIdentifier: "P0",
+                playingCard: JOKER
+            },
+            {
+                playerIdentifier: "P1",
+                playingCard: SPADE_3
+            },
+            {
+                playerIdentifier: "P2",
+                playingCard: SPADE_2
+            },
+            {
+                playerIdentifier: "P3",
+                playingCard: SPADE_4
+            },
+        ]
+        const expectedTurnResult: TurnResult = {
+            winner: "P3",
+            wonCardsByPlayer: [
+                {
+                    playerIdentifier: "P0",
+                    wonCards: [JOKER]
+                },
+                {
+                    playerIdentifier: "P1",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P2",
+                    wonCards: []
+                },
+                {
+                    playerIdentifier: "P3",
+                    wonCards: [SPADE_3, SPADE_2, SPADE_4]
+                },
+            ]
+        }
+        expect(resolveTarotTurn(playedCards)).toEqual(expectedTurnResult);
+    });
+})
--- src/domain/tarot-game/functions/tarot-turn-resolver.ts
+++ src/domain/tarot-game/functions/tarot-turn-resolver.ts
@@ -1,7 +1,85 @@
+import {PlayedCard, TurnResult} from "../../card-game/functions/resolve-turn";
+import {Face, PlayingCardType, Suit} from "tarot-card-deck/dist/cards/playing-card";
 import {PlayingCard} from "tarot-card-deck";
 import {PlayerIdentifier} from "../../card-game/player/card-game-player";
-import {TurnResult} from "../../card-game/functions/resolve-turn";
+import {JOKER} from "tarot-card-deck/dist/cards/all-playing-cards";
 
-export function resolveTarotTurn(playedCards: { playingCard: PlayingCard, playerIdentifier: PlayerIdentifier }[]): TurnResult{
-    return undefined;
+type ClassicCard = {
+    identifier: string;
+    value: number;
+    suit: Suit;
+    type: PlayingCardType.CLASSIC;
+} | {
+    identifier: string;
+    face: Face;
+    suit: Suit;
+    type: PlayingCardType.FACE;
+};
+
+type TrumpCard = {
+    identifier: string;
+    value: number;
+    type: PlayingCardType.TRUMP;
+}
+const faceValues = {
+    "J": 11,
+    "C": 12,
+    "Q": 13,
+    "K": 14
+}
+
+export function resolveTarotTurn(playedCards: readonly PlayedCard[]): TurnResult {
+    const trumpPlayed: boolean = playedCards.some(playedCard => playedCard.playingCard.type === PlayingCardType.TRUMP);
+    let winnerPlayedCard: PlayedCard;
+    if (trumpPlayed) {
+        winnerPlayedCard = [...playedCards]
+            .filter(playedCard => playedCard.playingCard.type === PlayingCardType.TRUMP)
+            .sort((a, b) => (b.playingCard as TrumpCard).value - (a.playingCard as TrumpCard).value)
+            [0]
+    } else {
+        const masterPlayedCard: PlayingCard = [...playedCards]
+            .filter(playedCard => playedCard.playingCard.type === PlayingCardType.CLASSIC || playedCard.playingCard.type === PlayingCardType.FACE)[0].playingCard
+        const masterSuit: Suit = (masterPlayedCard as ClassicCard).suit;
+
+        winnerPlayedCard = [...playedCards]
+            .filter(playedCard => playedCard.playingCard.type === PlayingCardType.CLASSIC || playedCard.playingCard.type === PlayingCardType.FACE)
+            .filter(playedCard => (playedCard.playingCard as ClassicCard).suit === masterSuit)
+            .sort((a, b) => sortClassicCards(a.playingCard as ClassicCard, b.playingCard as ClassicCard))
+            [0]
+    }
+
+    const players: PlayerIdentifier[] = playedCards.map(playedCard => playedCard.playerIdentifier);
+
+    const excuseIndex: number = playedCards.findIndex(playedCard => playedCard.playingCard === JOKER);
+    const playerThatPlayedExcuse: PlayerIdentifier = excuseIndex > -1 ? playedCards[excuseIndex].playerIdentifier : undefined;
+
+    return {
+        winner: winnerPlayedCard.playerIdentifier,
+        wonCardsByPlayer: players.map(currentPlayer => {
+            if (currentPlayer === winnerPlayedCard.playerIdentifier) {
+                return {
+                    playerIdentifier: currentPlayer,
+                    wonCards: playedCards.map(playedCard => playedCard.playingCard).filter(playingCard => playingCard !== JOKER)
+                }
+            } else if (currentPlayer === playerThatPlayedExcuse) {
+                return {
+                    playerIdentifier: currentPlayer,
+                    wonCards: playedCards.map(playedCard => playedCard.playingCard).filter(playingCard => playingCard === JOKER)
+                }
+            } else {
+                return {
+                    playerIdentifier: currentPlayer,
+                    wonCards: []
+                }
+            }
+        })
+    }
+}
+
+function sortClassicCards(a: ClassicCard, b: ClassicCard): number {
+    return getValueFromCard(b) - getValueFromCard(a);
+}
+
+function getValueFromCard(card: ClassicCard): number {
+    return card.type === PlayingCardType.CLASSIC ? card.value : faceValues[card.face];
 }
